c7d487b15b0e33db716fc08ecf97b61a0bbfe053
Sanjana Ayshi <67552924+SanjanaAyshi@users.noreply.github.com>
2024-01-11 00:57:51 +0600

    module 22.5
    upladed

---
+++ module 22.5/accounts/urls.py
@@ -0,0 +1,11 @@
+
+from django.urls import path
+from .views import UserRegistrationView, UserLoginView, UserBankAccountUpdateView
+from . import views
+urlpatterns = [
+    path('register/', UserRegistrationView.as_view(), name='register'),
+    path('login/', UserLoginView.as_view(), name='login'),
+    path('logout/', views.userLogout, name='logout'),
+    # path('logout/', UserLogoutView.as_view(), name='logout'),
+    path('profile/', UserBankAccountUpdateView.as_view(), name='profile' )
+]
